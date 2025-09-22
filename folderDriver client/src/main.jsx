0c4a70bb927f740f98def7ecf91a702b8233479c
monu shah <148376091+monushah108@users.noreply.github.com>
2025-09-22 11:27:39 +0530

    Update main.jsx

--- folderDriver client/src/main.jsx
+++ folderDriver client/src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { Store } from "./store/index.js";
 
 const client_id =
-  "389965029020-2c71sgd5bku35i957c5al69uh1hb39pv.apps.googleusercontent.com";
+  
 
 createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={client_id}>
